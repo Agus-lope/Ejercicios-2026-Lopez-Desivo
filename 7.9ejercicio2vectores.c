6871a70636e33ed0b149e0120896e2db0506d356
Agus-lope <aguslopezdesivo@gmail.com>
2026-09-07 10:19:42 -0300

    Refactor vector input and calculation code

--- 7.9ejercicio2vectores.c
+++ 7.9ejercicio2vectores.c
@@ -5,22 +5,20 @@ int main() {
     int i, suma = 0;
     float promedio;
 
-    // Cargar datos en el vector
+    
     printf("Ingrese 5 numeros:\n");
     for (i = 0; i < 5; i++) {
         printf("Elemento %d: ", i + 1);
         scanf("%d", &vector[i]);
     }
 
-    // Sumar los elementos del vector
+    
     for (i = 0; i < 5; i++) {
         suma += vector[i];
     }
-
-    // Calcular promedio
     promedio = (float)suma / 5;
 
-    // Mostrar resultados
+    
     printf("\nSuma total: %d\n", suma);
     printf("Promedio: %.2f\n", promedio);
 
